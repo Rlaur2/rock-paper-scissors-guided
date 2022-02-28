e34b092d6326799041a6a6544960450356aa432e
pavel <plaureanopena@gmail.com>
2022-02-28 18:57:38 -0500

    Create function to choose between R,P or S

---
+++ rps.js
@@ -0,0 +1,12 @@
+let computerPlay = (playerChoice) => {
+    //Code to randomly choose between Rock, paper, or scissors
+    let rng = Math.floor(Math.random()*3);
+   if (rng === 0) {
+       aiChoice = 'Rock';
+   } else if (rng === 1) {
+       aiChoice = 'Paper';
+   } else {
+       aiChoice = 'Scissors';
+   }
+   return aiChoice
+}

f0c09f62f9370c5440152354207cfd585c4a1080
pavel <plaureanopena@gmail.com>
2022-02-28 23:26:30 -0500

    Add function to correct typecase of prompted choice

--- rps.js
+++ rps.js
@@ -12,7 +12,7 @@ let computerPlay = () => {
 }
 
 let computerSelection;
-//The actual RPS game is played here after invoking the computerPlay function
+//The actual RPS game is coded here
 let round = (playerChoice,computerSelection) => {
     computerSelection = computerPlay();
     if (playerChoice === 'Rock' && computerSelection === 'Scissors' || playerChoice === 'Paper' && computerSelection === 'Rock' || playerChoice === 'Scissors' && computerSelection === 'Paper') {
@@ -28,8 +28,26 @@ let round = (playerChoice,computerSelection) => {
         console.log('Pick between "Rock","Paper", or "Scissors"!')
         //In the future I might have to put an if statement here saying 'if play count > 0 then substract one' or something similar.
         //or alternatively, all of these if statements can increase play count except for this last one.
+        result = null;
     }
     return result;
 }
+//Function to correct type case (lower case, all CAPS etc.)
+let caseFix = (word) => {
+    wordLower = word.toLowerCase();
+    wordFirst = wordLower.slice(0,1);
+    wordFirstUpper = wordFirst.toUpperCase();
+    final = wordLower.replace(wordFirst, wordFirstUpper);
+    return final;
+}
 
 
+//The full game code here that plays 5 times and states the winner 
+//I want to include a condition that if round === null then substract 1 from i (if i > 0) but I'm not sure how to do that.
+let game = () => {
+    for (let i = 0; i < 5; i++) {
+        playerChoice = prompt('Choose between "Rock, Paper, or Scissors"');
+        playerChoice = caseFix(playerChoice);
+        round(playerChoice,computerSelection);
+    }
+}
